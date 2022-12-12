fe2ea3cae38482686505666fd066c4701510a574
Casper Daris <daris@d-tt.nl>
2022-12-12 20:51:33 +0100

    Initial Commit

---
+++ Market/MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketApp.swift
+//  Market
+//
+//  Created by Casper on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
